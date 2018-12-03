c29c02e67f25439d1c0ca836287134461b40df38
Yukiji Nakatsuka <lunarbase@outlook.com>
2018-12-03 15:14:04 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+
+def badge_maker(string)
+    "Hello, my name is #{string}."
+end
+
+
+def batch_badge_creator(names)
+  names.map do |name|
+    "Hello, my name is #{name}."
+  end
+end
+
+
+def assign_rooms(names)
+  messages = []
+  names.each.with_index(1) do |name, index|
+    messages << "Hello, #{name}! You'll be assigned to room #{index}!"
+  end
+  messages
+end
+
+
+def printer(names)
+  puts batch_badge_creator(names)
+  assign_rooms(names)
+end
+
+
